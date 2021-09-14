96839f5fd712e7ad83c111902b54b756fa94f4a6
Miju Park <mijupark@Mijuui-MacBookPro.local>
2021-09-14 10:40:05 +0900

    feat: login form

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,7 +1,68 @@
 import React from "react";
+import { useForm } from "react-hook-form";
+
+interface ILoginForm {
+  email: string;
+  password: string;
+}
 
 const Login = () => {
-  return <div>Login</div>;
+  const {
+    register,
+    getValues,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<ILoginForm>();
+  const onSubmit = () => {
+    console.log(getValues());
+  };
+  return (
+    <div className="h-screen flex items-center justify-center bg-gray-800">
+      <div className="bg-white w-full max-w-lg pt-10 pb-7 rounded-lg text-center">
+        <h3 className="text-3xl text-gray-800">LogIn</h3>
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="grid gap-3 mt-5 px-5"
+        >
+          <input
+            {...register("email", {
+              required: "Email is required",
+            })}
+            name="email"
+            placeholder="Email"
+            className=" input mb-3"
+          />
+          {errors.email?.message && (
+            <span className="font-medium text-red-500">
+              {errors.email?.message}
+            </span>
+          )}
+          <input
+            {...register("password", {
+              required: "Password is required",
+              minLength: 10,
+            })}
+            name="password"
+            type="password"
+            placeholder="Password"
+            className="input"
+          />
+          {errors.password?.message && (
+            <span className="font-medium text-red-500">
+              {errors.password?.message}
+            </span>
+          )}
+          {errors?.password?.type === "minLength" && (
+            <span className="font-medium text-red-500">
+              Password should match minLength
+            </span>
+          )}
+
+          <button className="btn mt-3">Log In</button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
