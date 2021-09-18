5f8a37160eae15cf66f790570ed4d2e15cdb814a
Miju Park <gi6238@naver.com>
2021-09-18 17:21:30 +0900

    feat: categories

--- src/pages/client/restaurants.tsx
+++ src/pages/client/restaurants.tsx
@@ -48,8 +48,35 @@ export const Restaurants = () => {
       },
     },
   });
-  console.log(data);
-  return <h1>Restaurant</h1>;
+  return (
+    <div>
+      <form className="bg-gray-800 w-full py-40 flex items-center justify-center">
+        <input
+          className="input rounded-md border-0 w-3/12"
+          type="Search"
+          placeholder="Search Restaurants..."
+        />
+      </form>
+      {!loading && (
+        <div className="max-w-screen-2xl mx-auto mt-8">
+          <div className="flex justify-around max-w-sm mx-auto">
+            {data?.allCategories?.categories?.map((category) => (
+              <div className="flex flex-col items-center">
+                <div
+                  className="w-14 h-14 bg-cover hover:bg-gray-100 cursor-pointer rounded-full"
+                  style={{ backgroundImage: `url(${category.coverImg})` }}
+                ></div>
+                <span className="mt-1 text-sm text-center font-bold">
+                  {category.name}
+                </span>
+              </div>
+            ))}
+          </div>
+        </div>
+      )}
+      <div></div>
+    </div>
+  );
 };
 
 export default Restaurants;
