d20be0bca408e44b1f624b851f02d5c2143038ae
Miju Park <gi6238@naver.com>
2021-09-22 19:48:58 +0900

    feat: restaurant

--- src/components/restaurant.tsx
+++ src/components/restaurant.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 interface IRestaurantProps {
   id: string;
@@ -7,20 +8,23 @@ interface IRestaurantProps {
   categoryName?: string;
 }
 export const Restaurant: React.FC<IRestaurantProps> = ({
+  id,
   coverImg,
   name,
   categoryName,
 }) => {
   return (
-    <div className="flex flex-col">
-      <div
-        style={{ backgroundImage: `url(${coverImg})` }}
-        className="bg-cover bg-center mb-3 gap-11 py-28"
-      ></div>
-      <h3 className="text-xl font-medium">{name}</h3>
-      <span className="border-t mt-4 py-2 text-xs opacity-50 border-gray-400">
-        {categoryName}
-      </span>
-    </div>
+    <Link to={`/restaurant/${id}`}>
+      <div className="flex flex-col">
+        <div
+          style={{ backgroundImage: `url(${coverImg})` }}
+          className="bg-cover bg-center mb-3 gap-11 py-28"
+        ></div>
+        <h3 className="text-xl font-medium">{name}</h3>
+        <span className="border-t mt-4 py-2 text-xs opacity-50 border-gray-400">
+          {categoryName}
+        </span>
+      </div>
+    </Link>
   );
 };
--- src/routes/logged-in-router.tsx
+++ src/routes/logged-in-router.tsx
@@ -16,6 +16,7 @@ import ConfirmEmail from "../pages/user/confirm-email";
 import EditProfile from "../pages/user/edit-profile";
 import { Search } from "../pages/client/search";
 import { Category } from "../pages/client/category";
+import { RestaurantDetail } from "../pages/client/restaurant";
 
 const ClientRoutes = [
   <Route key={1} path="/" exact>
@@ -32,6 +33,9 @@ const ClientRoutes = [
   </Route>,
   <Route key={5} path="/category/:slug">
     <Category />
+  </Route>,
+  <Route key={6} path="/restaurant/:id">
+    <RestaurantDetail />
   </Route>,
 ];
 
